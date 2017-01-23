a0525c97a6d063a41d79a69a90a7e51fb47ae005
Wesley Shall <wsshall@oakland.edu>
2017-01-23 12:52:48 -0500

    Added new, empty BeerSong class

---
+++ BeerSong.java
@@ -0,0 +1,14 @@
+
+public class BeerSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void Ninety_Nine_Bottles_of_Beer(int num)
+	{
+	
+	}
+
+}
